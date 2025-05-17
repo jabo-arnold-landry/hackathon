2a21734e26e81a3fb26bc0e4d99cd960732aa0ce
jabo arnold landry <arnoldjab@gmail.com>
2025-05-17 16:37:36 +0200

    making API availible to frontend

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ const express = require("express");
 const env = require("dotenv").config();
 //getting the connection and connecting to a db
 const dbConnection = require("./config/dbconnection");
+const cors = require("cors");
 //establishing the connection
 dbConnection();
+
 const app = express();
 const port = process.env.PORT;
 app.use(express.json());
 app.use(express.urlencoded({ extends: true }));
+app.use(cors());
 //user login routes
 app.use("/credentials", require("./UserRoutes/AuthanticationRoutes"));
 //complain routes
